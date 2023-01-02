fccb5ccaceebf5b2f3f971f57cf07d1847780e1d
Danemous1985 <s3578854@student.rmit.edu.au>
2023-01-03 00:12:06 +1100

    Updating Exercises_3

---
+++ Exercises_3/3_2_greetings.py
@@ -0,0 +1,7 @@
+friends = ['mark','gary','tanya','cyrus','madison']
+
+print(f"Hi {friends[0].title()}, this is a message.")
+print(f"Hi {friends[1].title()}, this is a message.")
+print(f"Hi {friends[2].title()}, this is a message.")
+print(f"Hi {friends[3].title()}, this is a message.")
+print(f"Hi {friends[4].title()}, this is a message.")
---
+++ Exercises_3/3_3_your_own_list.py
@@ -0,0 +1,6 @@
+favourite_cars = ['mustang','s14','tesla model y','porsche 911 gt3']
+
+print(f"I would like to own a {favourite_cars[3].title()} because of its tech")
+print(f"I would like to own a EV such as a {favourite_cars[2].title()}")
+print(f"I like muscle cars such as {favourite_cars[0].title()}")
+print(f"An {favourite_cars[1].title()} would be nice")
